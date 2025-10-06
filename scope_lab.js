fd25679024ceafed3085633d07c23185225e6ee0
Odukogbe Adewale <oduswale1@gmail.com>
2025-10-06 13:50:08 +0100

    Add scope_lab.js file

--- scope_lab.js
+++ scope_lab.js
@@ -10,3 +10,8 @@ var blockVar = "I'm a block-scoped var";
 let blockLet = "I'm a block-scoped let";
 const blockConst = "I'm a block-scoped const";
 }
+
+// Global scope
+console.log(globalVar); // Output: "I'm a global variable"
+console.log(globalLet); // Output: "I'm also global, but scoped with let"
+console.log(globalConst); // Output: "I'm a global constant"
